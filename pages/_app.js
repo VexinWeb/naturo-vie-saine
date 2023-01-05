0bac4e07ccc567f6ade5716f77bc3d37f857ea8b
Julien Verley <mjules@gmail.com>
2023-01-05 09:37:37 +0100

    _app.js Layout.js Layout.module.css

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Layout from "../components/Layout";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Head>
+        <title>Naturopathie</title>
+        <meta name="Naturopathie" content="Naturopathie" />
+      </Head>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }

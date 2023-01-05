456592a8ea7153c93c06787d46f0c353e2d10f07
Julien Verley <mjules@gmail.com>
2023-01-05 10:19:51 +0100

    Modif Calendly function convertie en arrow function

--- components/Calendly.js
+++ components/Calendly.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import { InlineWidget } from 'react-calendly';
+import React from "react";
+import { InlineWidget } from "react-calendly";
 
-function MyCalendly() {
-    return (
-        <InlineWidget apiKey={process.env.REACT_APP_CALENDLY_APIKEY} url="https://calendly.com/marcoguzmandev" />
-    );
-}
+const MyCalendly = () => {
+  return (
+    <InlineWidget
+      apiKey={process.env.REACT_APP_CALENDLY_APIKEY}
+      url="https://calendly.com/marcoguzmandev"
+    />
+  );
+};
 
 export default MyCalendly;

6f1c9cc83532ce17b6c85dffd5d8425fa60a9991
Julien Verley <mjules@gmail.com>
2023-01-06 15:05:32 +0100

    Header logo fonts

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,28 @@
 import headerStyles from "../styles/Header.module.scss";
 import Link from "next/link";
 import Nav from "./Nav";
+import Image from "next/image";
+import { IBM_Plex_Sans_Devanagari } from "@next/font/google";
 
 const Header = () => {
   return (
     <div className={headerStyles.header}>
       <div className={headerStyles.headerLeft}>
-        <div>Logo</div>
-        <div>Anne Trébout Naturopathe</div>
+        <div className={headerStyles.headerImageContainer}>
+          {/* <div> */}
+          <Image
+            src="/ginkgo_white.png"
+            alt="logo"
+            style={{
+              maxWidth: "100%",
+              maxHeight: "100%",
+              objectFit: "contain",
+            }}
+            width={70}
+            height={70}
+          />
+        </div>
+        <div className={headerStyles.headerFonts}>Anne Trébout Naturopathe</div>
       </div>
       <Nav />
     </div>

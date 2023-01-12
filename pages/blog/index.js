de6da01dd8288e0901b31009fce6844bfbfd2ecf
Julien Verley <mjules@gmail.com>
2023-01-12 16:26:17 +0100

    pages blog/index.js : récupération des données

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,47 +1,97 @@
+import React from "react";
 import Link from "next/link";
 import { gql } from "@apollo/client";
-import client  from "../../apollo-client"
+import client from "../../apollo-client";
+import htmlParser from "../utils/htmlParser";
+import Image from "next/image";
 
-function blog( { data }) {
-    const posts = data.posts.edges.map((post) => post.node);
+export async function getStaticProps() {
+  const { data } = await client.query({
+    query: gql`
+      query {
+        posts {
+          edges {
+            node {
+              id
+              title
+              content
+              date
+            }
+          }
+        }
+      }
+    `,
+  });
+  return {
+    props: {
+      data,
+    },
+  };
+}
+
+function blog({ data }) {
+  const posts = data.posts.edges.map((post) => [
+    post.node.id,
+    post.node.title,
+    htmlParser(post.node.content),
+  ]);
+  let postsConverted = [];
+
+  posts.forEach((element) => {
+    postsConverted.push({
+      id: element[0],
+      title: element[1],
+      content: element[2],
+    });
+  });
+
+  console.log(postsConverted);
+
+  {
+    /* posts.map((post) => {
+      //   console.log(post.content); // Ok sans le title (h1)
+      const elementsArray = htmlParser(post.content);
+      console.log(elementsArray); // OK
+    }); */
     return (
-        <>
-            <h1>Blog alimenté par Wordpress</h1>
-            <ul>
-                {posts.map((post) => (
-                    <li key={post.id}>
-                        <Link href="/blog/[id]" as={`/blog/${post.id}`}>
-                            <h1>{post.title}</h1>
-                        </Link>
-                    </li>
-                ))}
-            </ul>
-        </>
-    )
+      <>
+        {/* <h1>Blog alimenté par Wordpress</h1> */}
+        <div>
+          {postsConverted.map((post) => (
+            <div
+              key={post.id}
+              style={{
+                width: "300px",
+                height: "300px",
+                backgroundColor: "grey",
+                margin: "50px",
+              }}
+            >
+              <Link href="/blog/[id]" as={`/blog/${post.id}`}>
+                <h2 style={{ padding: "10px" }}>{post.title}</h2>
+                {post.content.map((element) => {
+                  return React.createElement(
+                    element.tag,
+                    { key: element.text },
+                    element.text
+                  );
+                })}
+              </Link>
+            </div>
+          ))}
+        </div>
+        {/* <div>
+          {elementsArray.map((element, index) => {
+            return React.createElement(
+              element.tag,
+              { key: index },
+              element.text
+            );
+          })}
+        </div> */}
+      </>
+    );
+  }
 }
 
 export default blog;
-
-export async function getStaticProps() {
-    const { data } = await client.query({
-        query: gql`
-            query {
-                posts {
-                    edges {
-                        node {
-                            id
-                            title
-                            content
-                            date
-                        }
-                    }
-                }
-            }
-        `,
-    });
-    return {
-        props: {
-            data,
-        },
-    };
-}

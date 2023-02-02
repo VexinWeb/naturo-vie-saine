9c849d89c10877c7afa923fe7a2fd105ad252d2a
Julien Verley <mjules@gmail.com>
2023-02-02 11:01:16 +0100

    Footer logo VexinWEB

--- components/Footer.js
+++ components/Footer.js
@@ -5,7 +5,7 @@ import Image from "next/image";
 const Footer = () => {
   return (
     <div className={footerStyles.footer}>
-      <div>
+      <div className={footerStyles.left}>
         {/* <div>Téléphone : 06 06 06 06 06</div> */}
         <div>
           <a href="mailto:anne.trebout.naturo@gmail.com">
@@ -13,22 +13,33 @@ const Footer = () => {
           </a>
         </div>
       </div>
-      <div>
-        <div>
-          <a href="https://vexinweb.fr/">Site réalisé par VexinWeb</a>
-          {" | "}
-          <a href="https://www.linkedin.com/in/julienverley">
-            {" "}
-            Frontend Julien Verley
-          </a>
-          {" | "}
-          <a href="https://www.linkedin.com/in/marco-guzman-dev">
-            Backend Marco Guzman
-          </a>
-        </div>
-        <div></div>
+      {/* <div> */}
+      <div className={footerStyles.right}>
+        <a href="https://vexinweb.fr/">
+          Site réalisé par l&apos;agence
+          <Image
+            href="https://vexinweb.fr/"
+            src="/logo_vw.png"
+            alt="Logo VexinWEB"
+            style={{
+              maxWidth: "100%",
+              maxHeight: "100%",
+              objectFit: "contain",
+            }}
+            width={50}
+            height={50}
+          ></Image>
+        </a>
+        {/* <a href="https://www.linkedin.com/in/julienverley">
+          Développeur Frontend Julien Verley
+        </a>
+        <a href="https://www.linkedin.com/in/marco-guzman-dev">
+          Développeur Backend Marco Guzman
+        </a> */}
       </div>
+      <div></div>
     </div>
+    // </div>
   );
 };
 
